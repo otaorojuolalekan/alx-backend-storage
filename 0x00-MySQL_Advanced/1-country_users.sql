7b257ef8b94ecf8d2e414a88f94e429c39d98e54
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-10-17 14:52:07 +0100

    fixed task 1 -- missing name

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     country ENUM ('US', 'CO', 'TN') DEFAULT 'US',
     PRIMARY KEY(id)
 );
