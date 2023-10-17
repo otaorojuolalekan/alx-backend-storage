230e5562328588f357178365b82222130c4cd236
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-10-17 14:18:46 +0100

    fixed syntax error

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- task 1
 -- testing sql enumeration
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT('US'),
